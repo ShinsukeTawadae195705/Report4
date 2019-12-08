5c868e5510ef064b2ce10af9e8daa6faf0cb4d39
Shinsuke Tawada <e195705@ie.u-ryukyu.ac.jp>
2019-12-08 12:54:24 +0900

    step2

--- src/Main/Report4.java
+++ src/Main/Report4.java
@@ -1,6 +1,11 @@
 public class Report4 {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }catch (NullPointerException e){//ここでのeは任意の文字
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
